54a1b1d349a7b7819e82075ca139b4929014c834
ldatareserve <lnowlin@mail.com>
2024-04-26 23:18:32 -0400

    update

--- main.tf
+++ main.tf
@@ -38,3 +38,28 @@ resource "google_storage_bucket" "demo-bucket-1" {
     }
   }
 }
+resource "google_compute_network" "auto-vpc-tf" {
+  name = "auto-vpc-tf"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "sub-sg" {
+  name ="sub-sg"
+  network = google_compute_network.auto-vpc-tf.id
+  ip_cidr_range = "10.72.1.0/24"
+  region = "asia-northeast1"
+}
+
+
+#resource "google_compute_network" "custom-vpc-tf" {
+  #name = "custom-vpc-tf"
+ #auto_create_subnetworks = false
+#}
+
+output "auto" {
+  value = google_compute_network.auto-vpc-tf.id
+}
+
+#output "custom" {
+#  value = google_compute_network.custom-vpc-tf.id
+#}
